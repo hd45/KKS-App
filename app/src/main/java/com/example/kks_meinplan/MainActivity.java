42f8ae0f4acfe8b7e5f91ecd642d144793d19d85
hd45 <hdi@users.noreply.github.com>
2020-01-02 12:50:11 +0100

    app commit

--- app/src/main/java/com/example/kks_meinplan/MainActivity.java
+++ app/src/main/java/com/example/kks_meinplan/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.kks_meinplan;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -20,16 +22,19 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigatonView = findViewById(R.id.nav_view);
+
+        setSupportActionBar(toolbar);
+
         navigatonView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
@@ -42,10 +47,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     new VertretungsplanFragment()).commit();
             navigatonView.setCheckedItem(R.id.nav_vertretungsplan);
         }
+
+
     }
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
 
         switch (menuItem.getItemId()) {
             case R.id.nav_vertretungsplan:
@@ -69,6 +78,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         new EinstellungenFragment()).commit();
                 break;
         }
+
         drawer.closeDrawer(GravityCompat.START);
 
         return true;
@@ -76,10 +86,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+
         } else {
-            super.onBackPressed();
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("Möchtest du die App verlassen?");
+            alertDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("Ja",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    moveTaskToBack(true);
+                                    android.os.Process.killProcess(android.os.Process.myPid());
+                                    System.exit(1);
+                                }
+                            })
+
+                    .setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
         }
+
+
+        //todo 1 logout alert dialog
+        //todo 2 switches Code ist richtig
+        // aber irgendwas ist falsch vllt mit OnCreate lifecycle
+
     }
 }

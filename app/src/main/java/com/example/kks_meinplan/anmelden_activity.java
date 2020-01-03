9e5739188bb64674a449ea28247ec49a24e6f925
hd45 <hdi@users.noreply.github.com>
2020-01-03 17:36:09 +0100

    anmelden activity commit

--- app/src/main/java/com/example/kks_meinplan/anmelden_activity.java
+++ app/src/main/java/com/example/kks_meinplan/anmelden_activity.java
@@ -2,7 +2,10 @@ package com.example.kks_meinplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class anmelden_activity extends AppCompatActivity {
     //todo layout erstellen und Weiterleitung an die HauptActivity
@@ -10,5 +13,19 @@ public class anmelden_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anmelden);
+
+        Button button = findViewById(R.id.anmelden);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainactivity();
+            }
+        });
+    }
+
+
+    public void openMainactivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

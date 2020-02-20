8282773761af5e3cf3a814f1e0489443f07365b5
hd45 <hdi@users.noreply.github.com>
2020-02-20 12:26:15 +0100

    anmelden activity

--- app/src/main/java/com/example/kks_meinplan/StartActivity.java
+++ app/src/main/java/com/example/kks_meinplan/StartActivity.java
@@ -11,7 +11,7 @@ public class StartActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_anmelden);
+        setContentView(R.layout.activity_start);
 
 
 

576149cbffd0de60efc7f7b67cd9d9684c1501c0
hd45 <hdi@users.noreply.github.com>
2020-03-05 15:03:23 +0100

    Versuch: Vertretung Activity (files added)

---
+++ app/src/main/java/com/example/kks_meinplan/VertretungTest.java
@@ -0,0 +1,49 @@
+package com.example.kks_meinplan;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VertretungTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vertretung_test);
+
+        final TextView textView = findViewById(R.id.textView);
+
+        Button lesenBtn = findViewById(R.id.lesenBtn);
+        lesenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Lesen", "Klick");
+                new ActivityDataSource(textView).execute("someParams");
+            }
+
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drawer_menu, menu); // todo hier nicht sicher: menu oder drawer menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_vertretungsplan) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}

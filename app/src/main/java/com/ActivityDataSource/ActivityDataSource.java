5787614b0768d10fc32835ed572b4b55c61e7119
hd45 <hdi@users.noreply.github.com>
2020-03-05 09:39:30 +0100

    ActivityDataSource Package erstellt

---
+++ app/src/main/java/com/ActivityDataSource/ActivityDataSource.java
@@ -0,0 +1,11 @@
+package com.ActivityDataSource;
+
+import android.os.AsyncTask;
+
+public class ActivityDataSource extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params){
+        return null;
+    }
+}

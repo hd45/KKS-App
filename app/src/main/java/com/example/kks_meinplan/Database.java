83cd02c27ecf9f303bc8d5703d0e679284acde60
hd45 <hdi@users.noreply.github.com>
2020-03-06 05:19:03 +0100

    Database demo

---
+++ app/src/main/java/com/example/kks_meinplan/Database.java
@@ -0,0 +1,16 @@
+package com.example.kks_meinplan;
+
+public class Database {
+
+    public static String[] codes ={
+            "asdfjklö",
+            "123456",
+            "gfjklödnm",
+            "tdfkh",
+            "resdj",
+            "ftfbgve",};
+
+}
+
+// wird nur zu Testzwecken gebraucht
+// Wird später durch die Datenbank ersetzt

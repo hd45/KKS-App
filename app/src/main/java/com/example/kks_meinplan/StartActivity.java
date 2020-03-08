5746fac3f54ea8b9b61dc8b9e2aa9e462592e8c9
hd45 <hdi@users.noreply.github.com>
2020-03-08 14:34:42 +0100

    login Passwort Überprüfung

--- app/src/main/java/com/example/kks_meinplan/StartActivity.java
+++ app/src/main/java/com/example/kks_meinplan/StartActivity.java
@@ -6,21 +6,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
+
+    EditText passwort;
+    Button anmelden;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        Button button = findViewById(R.id.anmelden);
-        button.setOnClickListener(new View.OnClickListener() {
+        passwort = findViewById(R.id.passwort);
+        anmelden = findViewById(R.id.anmelden);
+
+        anmelden.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openMainactivity();
-                finish();
+                String code = passwort.getText().toString();
+                if (!code.equals("")) {
+                    for (int i = 0; i < Database.codes.length; i++) {
+                        if (code.equals(Database.codes[i])) {
+                            //enter the app
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(StartActivity.this, "Herzlich Willkommen", Toast.LENGTH_SHORT).show();
+                            break;
+                        } else {
+                            if (i == (Database.codes.length - 1) && !code.equals(Database.codes[i])) {
+                                Toast.makeText(StartActivity.this, "Falsches Passwort. Bitte versuche es erneut", Toast.LENGTH_SHORT).show();
+                                break;
+                            }
+                        } //end else
+                    } //end for
+                } //end if
             }
         });
+    }
+
+    public void onClick(View v) {
+        openMainactivity();
+        finish();
     }
 
 
